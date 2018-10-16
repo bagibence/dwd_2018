1a13db2451eb3b5dce7f827d2f017ee64dd26cfd
Bence Bagi <bagi.bence94@gmail.com>
2018-10-16 21:20:49 +0200

    If the station has no measurements (aka mes.empty), skip adding it to the database because it throws a keyerror otherwise

--- database_setup.py
+++ database_setup.py
@@ -6,14 +6,15 @@ import os
 
 # set up connection to the database
 # edit this when working on the server
-db.set_up_connection(db.db, 'weather')
+db.set_up_connection(db.db, 'bence_test')
 
 # insert stations
 stations_df = station_names.get_stations_dataframe()
 db.insert_into_table(stations_df, 'Station')
 
 # get daily measurement data
-userpath = os.path.dirname(os.path.realpath(__file__))
+#userpath = os.path.dirname(os.path.realpath(__file__))
+userpath = '/local/data_dwd/october2018'
 #get_data.get_data(userpath, historical=True, recent=True,
 #                  hourly=False, verbose=True)
 
@@ -26,5 +27,8 @@ for i, s_id in enumerate(stations_df.index):
         print('something went wrong processing station: {}'.format(s_id))
         print(e)
     else:
-        db.insert_into_table(mes, 'DailyMeasurement')
-        print('{}: {}'.format(i, s_id))
+        if not mes.empty:
+            db.insert_into_table(mes, 'DailyMeasurement', overwrite=True)
+            print('{}: {}'.format(i, s_id))
+        else:
+            print('{}: {} was empty'.format(i, s_id))
