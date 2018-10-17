287653f649bbbd445ef81e84978a84b26cedb0e0
Bence Bagi <bagi.bence94@gmail.com>
2018-10-17 21:09:54 +0200

    Add db_session context so that we don't open a different session for every station. Might make the insertion faster

--- database_setup.py
+++ database_setup.py
@@ -6,7 +6,7 @@ import os
 
 # set up connection to the database
 # edit this when working on the server
-db.set_up_connection(db.db, 'bence_test')
+db.set_up_connection(db.db, 'bence_test', create_tables=True)
 
 # insert stations
 stations_df = station_names.get_stations_dataframe()
@@ -19,16 +19,17 @@ userpath = '/local/data_dwd/october2018'
 #                  hourly=False, verbose=True)
 
 # insert measurement data
-print('inserting measurement data into the database...')
-for i, s_id in enumerate(stations_df.index):
-    try:
-        mes = process_data.process_data(userpath, s_id, 'daily')
-    except BaseException as e:
-        print('something went wrong processing station: {}'.format(s_id))
-        print(e)
-    else:
-        if not mes.empty:
-            db.insert_into_table(mes, 'DailyMeasurement', overwrite=True)
-            print('{}: {}'.format(i, s_id))
+with db.porm.db_session:
+    print('inserting measurement data into the database...')
+    for i, s_id in enumerate(stations_df.index):
+        try:
+            mes = process_data.process_data(userpath, s_id, 'daily')
+        except BaseException as e:
+            print('something went wrong processing station: {}'.format(s_id))
+            print(e)
         else:
-            print('{}: {} was empty'.format(i, s_id))
+            if not mes.empty:
+                db.insert_into_table(mes, 'DailyMeasurement', overwrite=True)
+                print('{}: {}'.format(i, s_id))
+            else:
+                print('{}: {} was empty'.format(i, s_id))
